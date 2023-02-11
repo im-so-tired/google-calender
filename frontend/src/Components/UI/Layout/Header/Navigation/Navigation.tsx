01821cfd7a763c2ee6ff03553bb3cbc6cea89df9
4118 Марков П.И <markov-pt@mail.ru>
2023-02-11 16:11:20 +0300

    feat: create currect navigate in header

--- frontend/src/Components/UI/Layout/Header/Navigation/Navigation.tsx
+++ frontend/src/Components/UI/Layout/Header/Navigation/Navigation.tsx
@@ -1,8 +1,6 @@
 import cn from 'classnames'
 import React, { FC } from 'react'
-import { Link, useHref, useLocation, useParams } from 'react-router-dom'
-
-import { useGetTimeZone } from '@/hooks/useGetTimeZone'
+import { Link } from 'react-router-dom'
 
 import { getGenitiveMonth } from '@/utils/date/genitiveMonth'
 
@@ -11,23 +9,20 @@ import { useNavigate } from './useNavigate'
 import CircleBtn from '@/common/Buttons/CircleBtn/CircleBtn'
 
 const Navigation: FC = () => {
-	const { day, month, year } = useParams()
-	useNavigate()
-	const timeZone = useGetTimeZone()
+	const { nextDay, previousDay, getDate } = useNavigate()
+	
 	return (
 		<nav className={styles.navigation}>
 			<Link to="/">
 				<button className={cn('navBtn', styles.todayBtn)}>Сегодня</button>
 			</Link>
-			<Link to={`${timeZone}/${year}/${month}/${Number(day) - 1}`}>
+			<Link to={previousDay()}>
 				<CircleBtn icon="MdArrowBackIosNew" size={32} iconSize={18} />
 			</Link>
-			<Link to={`${timeZone}/${year}/${month}/${Number(day) + 1}`}>
+			<Link to={nextDay()}>
 				<CircleBtn icon="MdArrowForwardIos" size={32} iconSize={18} />
 			</Link>
-			<h2 className={styles.selectedDate}>
-				{`${day} ${getGenitiveMonth(Number(month))} ${year}`}
-			</h2>
+			<h2 className={styles.selectedDate}>{getDate()}</h2>
 		</nav>
 	)
 }
--- frontend/src/Components/UI/Layout/Header/Navigation/useNavigate.ts
+++ frontend/src/Components/UI/Layout/Header/Navigation/useNavigate.ts
@@ -1,6 +1,55 @@
 import { useParams } from 'react-router-dom'
 
+import { useGetTimeZone } from '@/hooks/useGetTimeZone'
+
+import { getGenitiveMonth } from '@/utils/date/genitiveMonth'
+
 export const useNavigate = () => {
 	const { day, month, year } = useParams()
+	const timeZone = useGetTimeZone()
 
+	const dayInMonthIs30 = (value: number) =>
+		value === 4 || value === 6 || value === 9 || value === 11
+
+	const nextDay = () => {
+		let numDay = Number(day)
+		let numMonth = Number(month)
+		let numYear = Number(year)
+
+		if (dayInMonthIs30(numMonth) ? numDay !== 30 : numDay !== 31) {
+			numDay += 1
+		} else if (numMonth !== 12) {
+			numMonth += 1
+			numDay = 1
+		} else {
+			numYear += 1
+			numMonth = 1
+			numDay = 1
+		}
+		return `${timeZone}/${numYear}/${numMonth}/${numDay}`
+	}
+
+	const previousDay = () => {
+		let numDay = Number(day)
+		let numMonth = Number(month)
+		let numYear = Number(year)
+
+		if (numDay !== 1) {
+			numDay -= 1
+		} else if (numMonth !== 1) {
+			numMonth -= 1
+			numDay = dayInMonthIs30(numMonth) ? 30 : 31
+		} else {
+			numYear -= 1
+			numMonth = 12
+			numDay = dayInMonthIs30(numMonth) ? 30 : 31
+		}
+		return `${timeZone}/${numYear}/${numMonth}/${numDay}`
+	}
+
+	const getDate = () => {
+		return `${day} ${getGenitiveMonth(Number(month))} ${year}`
+	}
+
+	return { nextDay, previousDay, getDate }
 }
