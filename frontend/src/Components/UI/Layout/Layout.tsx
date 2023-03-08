fd703f322e9648eb87c7dac037f8adb5ededee7b
4118 Марков П.И <markov-pt@mail.ru>
2023-03-08 10:32:46 +0300

    feat: add animation for sidebar

--- frontend/src/Components/UI/Layout/Layout.tsx
+++ frontend/src/Components/UI/Layout/Layout.tsx
@@ -1,7 +1,9 @@
 import { FC, useCallback, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { Transition } from 'react-transition-group'
 
 import Header from './Header/Header'
+import styles from './Layout.module.scss'
 import Sidebar from './Sidebar/Sidebar'
 import { LayoutProvider } from './useLayoutContext'
 
@@ -12,14 +14,30 @@ const Layout: FC = () => {
 		setShowSidebar(prev => !prev)
 	}, [])
 
+	const transitionStyles: any = {
+		entering: { transform: 'translateX(-256px)' },
+		entered: { transform: 'translateX(0)' },
+		exiting: { transform: 'translateX(0)' },
+		exited: { transform: 'translateX(-256px)' },
+	}
+
 	return (
 		<div>
 			<LayoutProvider value={{ showSidebar, toggleSidebar }}>
 				<Header />
-				{showSidebar && <Sidebar />}
-				<main>
-					<Outlet />
-				</main>
+				<Transition in={showSidebar} timeout={0}>
+					{state => (
+						<div
+							className={styles.wrapper}
+							style={{ ...transitionStyles[state] }}
+						>
+							<Sidebar />
+							<main>
+								<Outlet />
+							</main>
+						</div>
+					)}
+				</Transition>
 			</LayoutProvider>
 		</div>
 	)
